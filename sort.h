dcc9560d5dfdba6f3dc953549b767550bfcd9f2a
SwiftFc <aseiduhenrydade@gmail.com>
2023-12-15 15:03:43 -0800

    new

--- sort.h
+++ sort.h
@@ -28,5 +28,9 @@ void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 
+/* Print_list and Print_array functions */
+void print_list(const listint_t *list);
+void print_array(const int *array, size_t size);
+
 
 #endif /* SORT_H */
